99be7c6c2cee77f5dc70af4316ac2e1eb1ff055b
Daniel Norman <daniel@2color.me>
2021-04-19 11:55:14 +0200

    Add performance instrumentation for prisma queries

--- src/context.ts
+++ src/context.ts
@@ -1,11 +1,24 @@
 import { PrismaClient } from '@prisma/client'
+import * as Sentry from '@sentry/node'
 
 export interface Context {
   prisma: PrismaClient
 }
 
 const prisma = new PrismaClient({
-  log: ['error', 'info', 'warn']
+  log: ['error', 'info', 'warn'],
+})
+
+// Middleware function to track db query performance
+prisma.$use(async (params, next) => {
+  const transaction = Sentry.startTransaction({
+    op: `${params.model}.${params.action}`,
+    name: 'Prisma DB query',
+  })
+  const result = await next(params)
+  transaction.finish()
+
+  return result
 })
 
 export const context: Context = {
